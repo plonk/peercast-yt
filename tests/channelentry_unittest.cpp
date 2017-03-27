2c63aaf55d343f04bc3013f9d38d057bab1587bd
Yoteichi <plonk@piano.email.ne.jp>
2017-03-28 03:29:20 +0900

    ChannelEntryのテストを更新。

--- tests/channelentry_unittest.cpp
+++ tests/channelentry_unittest.cpp
@@ -21,13 +21,28 @@ public:
     }
 };
 
+TEST_F(ChannelEntryFixture, constructor)
+{
+    ASSERT_THROW(ChannelEntry({}), std::runtime_error);
+}
+
 TEST_F(ChannelEntryFixture, textToChannelEntries)
 {
     auto vec = ChannelEntry::textToChannelEntries("予定地<>97968780D09CC97BB98D4A2BF221EDE7<>127.0.0.1:7144<>http://www.example.com/<>プログラミング<>peercastをいじる - &lt;Free&gt;<>-1<>-1<>428<>FLV<><><><><>%E4%BA%88%E5%AE%9A%E5%9C%B0<>1:14<>click<><>1\n");
 
     ASSERT_EQ(1, vec.size());
 
-    ASSERT_STREQ("予定地", vec[0].name.c_str());
-    ASSERT_EQ(428, vec[0].bitrate);
-    ASSERT_STREQ("FLV", vec[0].contentTypeStr.c_str());
+    auto& entry = vec[0];
+
+    ASSERT_STREQ("予定地", entry.name.c_str());
+    ASSERT_STREQ("97968780D09CC97BB98D4A2BF221EDE7", ((std::string) entry.id).c_str());
+    ASSERT_EQ(428, entry.bitrate);
+    ASSERT_STREQ("FLV", entry.contentTypeStr.c_str());
+    ASSERT_STREQ("peercastをいじる - &lt;Free&gt;", entry.desc.c_str());
+    ASSERT_STREQ("プログラミング", entry.genre.c_str());
+    ASSERT_STREQ("http://www.example.com/", entry.url.c_str());
+    ASSERT_STREQ("127.0.0.1:7144", entry.tip.c_str());
+    ASSERT_STREQ("1:14", entry.uptime.c_str());
+    ASSERT_EQ(-1, entry.numDirects);
+    ASSERT_EQ(-1, entry.numRelays);
 }
