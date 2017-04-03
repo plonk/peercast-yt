f4d7eed1079be1cad83a7318939df573cbc39346
Yoteichi <plonk@piano.email.ne.jp>
2017-04-04 08:47:22 +0900

    ChannelEntryの仕様変更に合わせてテストを変更。

--- tests/channelentry_unittest.cpp
+++ tests/channelentry_unittest.cpp
@@ -23,12 +23,12 @@ public:
 
 TEST_F(ChannelEntryFixture, constructor)
 {
-    ASSERT_THROW(ChannelEntry({}), std::runtime_error);
+    ASSERT_THROW(ChannelEntry({}, ""), std::runtime_error);
 }
 
 TEST_F(ChannelEntryFixture, textToChannelEntries)
 {
-    auto vec = ChannelEntry::textToChannelEntries("予定地<>97968780D09CC97BB98D4A2BF221EDE7<>127.0.0.1:7144<>http://www.example.com/<>プログラミング<>peercastをいじる - &lt;Free&gt;<>-1<>-1<>428<>FLV<><><><><>%E4%BA%88%E5%AE%9A%E5%9C%B0<>1:14<>click<><>1\n");
+    auto vec = ChannelEntry::textToChannelEntries("予定地<>97968780D09CC97BB98D4A2BF221EDE7<>127.0.0.1:7144<>http://www.example.com/<>プログラミング<>peercastをいじる - &lt;Free&gt;<>-1<>-1<>428<>FLV<><><><><>%E4%BA%88%E5%AE%9A%E5%9C%B0<>1:14<>click<><>1\n", "");
 
     ASSERT_EQ(1, vec.size());
 
