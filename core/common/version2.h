75e436142b9c5f66ea726fd100813a267cba3814
Yoteichi <plonk@piano.email.ne.jp>
2017-04-13 08:16:06 +0900

    バージョン番号を上げた。YT4

--- core/common/version2.h
+++ core/common/version2.h
@@ -34,11 +34,11 @@ static const int PCP_ROOT_VERSION       = 1218;
 
 static const int PCP_CLIENT_MINVERSION  = 1200;
 
-#define PCX_AGENT "PeerCast/0.1218 (YT3)"
-#define PCX_VERSTRING "v0.1218 YT3"
+#define PCX_AGENT "PeerCast/0.1218 (YT4)"
+#define PCX_VERSTRING "v0.1218 YT4"
 
 #define PCP_CLIENT_VERSION_EX_PREFIX "YT"
-static const int PCP_CLIENT_VERSION_EX_NUMBER = 3;
+static const int PCP_CLIENT_VERSION_EX_NUMBER = 4;
 // ------------------------------------------------
 
 #endif
