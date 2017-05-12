626eb940c2018b3906fec7e70407b0fb28a297eb
Yoteichi <plonk@piano.email.ne.jp>
2017-05-13 06:19:45 +0900

    初期状態の確認を追加した。

--- tests/channel_unittest.cpp
+++ tests/channel_unittest.cpp
@@ -21,6 +21,78 @@ public:
     }
 };
 
+TEST_F(ChannelFixture, initialState)
+{
+    Channel c;
+
+    // ::String            mount;
+    ASSERT_STREQ("", c.mount.cstr());
+    // ChanMeta            insertMeta;
+    // ChanPacket          headPack;
+
+    // ChanPacketBuffer    rawData;
+
+    // ChannelStream       *sourceStream;
+    ASSERT_EQ(nullptr, c.sourceStream);
+    // unsigned int        streamIndex;
+    ASSERT_EQ(0, c.streamIndex);
+
+    // ChanInfo            info;
+    // ChanHit             sourceHost;
+    // ChanHit             designatedHost;
+
+    // GnuID               remoteID;
+    ASSERT_FALSE(c.remoteID.isSet());
+
+    // ::String            sourceURL;
+    ASSERT_STREQ("", c.sourceURL.cstr());
+
+    // bool                bump, stayConnected;
+    ASSERT_FALSE(c.bump);
+    ASSERT_FALSE(c.stayConnected);
+    // int                 icyMetaInterval;
+    ASSERT_EQ(0, c.icyMetaInterval);
+    // unsigned int        streamPos;
+    ASSERT_EQ(0, c.streamPos);
+    // bool                readDelay;
+    ASSERT_FALSE(c.readDelay);
+
+    // TYPE                type;
+    ASSERT_EQ(Channel::T_NONE, c.type);
+    // ChannelSource       *sourceData;
+    ASSERT_EQ(nullptr, c.sourceData);
+
+    // SRC_TYPE            srcType;
+    ASSERT_EQ(Channel::SRC_NONE, c.srcType);
+
+    // MP3Header           mp3Head;
+    // ThreadInfo          thread;
+
+    // unsigned int        lastIdleTime;
+    ASSERT_EQ(0, c.lastIdleTime);
+    // STATUS              status;
+    ASSERT_EQ(Channel::S_NONE, c.status);
+
+    // ClientSocket        *sock;
+    ASSERT_EQ(nullptr, c.sock);
+    // ClientSocket        *pushSock;
+    ASSERT_EQ(nullptr, c.pushSock);
+
+    // unsigned int        lastTrackerUpdate;
+    ASSERT_EQ(0, c.lastTrackerUpdate);
+    // unsigned int        lastMetaUpdate;
+    ASSERT_EQ(0, c.lastMetaUpdate);
+
+    // double              startTime, syncTime;
+    ASSERT_EQ(0, c.startTime);
+    ASSERT_EQ(0, c.syncTime);
+
+    // WEvent              syncEvent;
+
+    // Channel             *next;
+    ASSERT_EQ(nullptr, c.next);
+}
+
 TEST_F(ChannelFixture, renderHexDump)
 {
     ASSERT_STREQ("",
