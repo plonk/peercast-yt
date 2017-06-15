7d755edf895ac79597957ddbf4f771d6a1d93051
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-06-15 18:33:56 +0900

    _ftimeを修正

--- core/win32/wsys.cpp
+++ core/win32/wsys.cpp
@@ -46,7 +46,7 @@ double WSys::getDTime()
 {
    struct _timeb timebuffer;
 
-   _ftime( &timebuffer );
+   _ftime_s( &timebuffer );
 
    return (double)timebuffer.time+(((double)timebuffer.millitm)/1000);
 }
