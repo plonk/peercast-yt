d1647c365c584a6d8c60b13b329ac6bb6c1ef173
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-06-15 18:33:18 +0900

    gmdateを修正

--- core/common/cgi.cpp
+++ core/common/cgi.cpp
@@ -100,20 +100,15 @@ static const char* monthNames[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "J
 std::string rfc1123Time(time_t t)
 {
     char fmt[30], buf[30];
-#if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _BSD_SOURCE || _SVID_SOURCE || _POSIX_SOURCE
     tm tm;
 
+#if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _BSD_SOURCE || _SVID_SOURCE || _POSIX_SOURCE
     gmtime_r(&t, &tm);
+#else
+    gmtime_s(&tm, &t);
+#endif
     strftime(fmt, sizeof(fmt), "%%s, %d %%s %Y %H:%M:%S GMT", &tm);
     std::snprintf(buf, sizeof(buf), fmt, daysOfWeek[tm.tm_wday], monthNames[tm.tm_mon]);
-#else
-    // FIXME: The code that follows should have a race!
-    struct tm* ptm;
-
-    ptm = gmtime(&t);
-    strftime(fmt, sizeof(fmt), "%%s, %d %%s %Y %H:%M:%S GMT", ptm);
-    std::snprintf(buf, sizeof(buf), fmt, daysOfWeek[ptm->tm_wday], monthNames[ptm->tm_mon]);
-#endif
 
     return buf;
 }
