7f723b27fee690b286dac6706ef8c57aa88eb54d
Yoteichi <plonk@piano.email.ne.jp>
2017-06-19 02:57:20 +0900

    Do not use gmtime_r on WIN32

--- core/common/cgi.cpp
+++ core/common/cgi.cpp
@@ -100,7 +100,7 @@ static const char* monthNames[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "J
 std::string rfc1123Time(time_t t)
 {
     char fmt[30], buf[30];
-#if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _BSD_SOURCE || _SVID_SOURCE || _POSIX_SOURCE
+#if !defined(WIN32) && (_POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _BSD_SOURCE || _SVID_SOURCE || _POSIX_SOURCE)
     tm tm;
 
     gmtime_r(&t, &tm);
