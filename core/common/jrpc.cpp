8cedec5fead8bc988a3b56bc19109af4f01afa81
Yoteichi <plonk@piano.email.ne.jp>
2017-07-19 18:05:12 +0900

    range_error の catch 節を削除した。

--- core/common/jrpc.cpp
+++ core/common/jrpc.cpp
@@ -41,18 +41,14 @@ json JrpcApi::call_internal(const string& input)
 
     try {
         id = j.at("id");
-    } catch (std::range_error&) {
-        return error_object(-32600, "Invalid Request");
     } catch (std::out_of_range) {
         return error_object(-32600, "Invalid Request");
     }
 
     try {
         method = j.at("method");
-    } catch (std::range_error&) {
-        return error_object(-32600, "Invalid Request", id);
     } catch (std::out_of_range) {
-        return error_object(-32600, "Invalid Request", id));
+        return error_object(-32600, "Invalid Request", id);
     }
 
     if (j.find("params") == j.end())
