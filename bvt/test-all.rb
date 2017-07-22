44b800b228cd3bffa4c5b7372e64d2cd1f3a49b9
Yoteichi <plonk@piano.email.ne.jp>
2017-07-23 04:56:07 +0900

    add bvt/

---
+++ bvt/test-all.rb
@@ -0,0 +1,10 @@
+Dir.glob("[0-9]*-*.rb").sort.each do |rb|
+  name = rb.sub(/\.rb$/, '')
+  print "#{name}: "
+  STDOUT.flush
+  system("ruby", rb)
+  unless $?.exitstatus == 0
+    puts "failed"
+    exit 1
+  end
+end
