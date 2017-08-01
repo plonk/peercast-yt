6d5b75405c311955e8fbde7a86211ebe5e75a600
Yoteichi <plonk@piano.email.ne.jp>
2017-08-01 21:18:59 +0900

    複数のURLに配信・録画できるようにした。

--- rtmp-server/rtmp-server.cpp
+++ rtmp-server/rtmp-server.cpp
@@ -8,6 +8,8 @@
 #include "uri.h"
 #include "iohelpers.h"
 #include "session.h"
+#include "splitter.h"
+#include "defer.h"
 
 namespace rtmpserver
 {
@@ -20,6 +22,36 @@ namespace rtmpserver
 }
 
 using namespace rtmpserver;
+
+Stream* openUri(const std::string& spec)
+{
+    URI uri(spec);
+
+    if (uri.scheme() == "http")
+    {
+        ClientSocket* sock = sys->createSocket();
+        Host host;
+        host.fromStrName(uri.host().c_str(), uri.port());
+        sock->open(host);
+        sock->connect();
+        sock->writeLineF("POST %s?%s HTTP/1.0", uri.path().c_str(), uri.query().c_str());
+        sock->writeLine("");
+        return sock;
+    }else if (uri.scheme() == "file")
+    {
+        FileStream *file = new FileStream();
+        file->openWriteReplace(uri.path().c_str());
+        return file;
+    }else if (!uri.isValid())
+    {
+        FileStream *file = new FileStream();
+        file->openWriteReplace(spec.c_str());
+        return file;
+    }else
+    {
+        throw std::runtime_error("Unsupported protocol " + uri.scheme());
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -39,8 +71,6 @@ int main(int argc, char* argv[])
     if (argc == 1)
         die("no URL supplied");
 
-    URI uri(argv[1]);
-
     Host serverHost(0 /* IGNORED */, rtmp_port);
     ClientSocket* server = sys->createSocket();
     server->bind(serverHost);
@@ -48,15 +78,20 @@ int main(int argc, char* argv[])
 
     while (true)
     {
+        // ループブロックの最後でストリームを解放する。
+        std::vector<Stream*> streams;
+        Defer cb([&]() { for (auto s : streams) delete s; });
+
         ClientSocket* client = server->accept();
 
-        assert(client != nullptr);
+        for (int i = 1; i < argc; ++i)
+            streams.push_back(openUri(argv[i]));
 
-        FileStream file;
-        file.openWriteReplace("out.flv");
-        FLVWriter writer(file);
+        StreamSplitter splitter(streams);
 
-        Session session(client, uri, writer);
+        FLVWriter writer(splitter);
+
+        Session session(client, writer);
         session.run();
         client->close();
     }
