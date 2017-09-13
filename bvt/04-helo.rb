84ec4f70c5404bd111461f77b2a25ca520eab986
Yoteichi <plonk@piano.email.ne.jp>
2017-09-13 10:32:50 +0900

    agnt 以外のアトムも検査するようにした。

--- bvt/04-helo.rb
+++ bvt/04-helo.rb
@@ -4,7 +4,7 @@ require 'socket'
 require_relative 'atom'
 include Atom
 
-def get_version(tip)
+def helo(tip)
   ip, port = tip.split(':')
 
   s = TCPSocket.new(ip, port.to_i)
@@ -19,17 +19,18 @@ def get_version(tip)
   # HELO
   s.write helo
   # OLEH
-  atom = Atom::read(s)
-  atom['agnt'] && atom['agnt'].value
+  return Atom::read(s)
 end
 
 spawn_peercast
 
-ver = get_version('127.0.0.1:7144')
+oleh = helo('127.0.0.1:7144')
 
-if ver =~ /\APeerCast\/0.1218 \(YT\d\d\)\z/
-  puts 'ok'
-else
-  STDERR.puts ver.inspect
-  exit 1
-end
+fail 'oleh' unless oleh.id4 == 'oleh'
+fail 'agnt' unless oleh['agnt'] && oleh['agnt'].value =~ /\APeerCast\/0.1218 \(YT\d\d\)\z/
+fail 'sid' unless oleh['sid']
+fail 'ver' unless oleh['ver'] && oleh['ver'].value == 1218
+fail 'rip' unless oleh['rip']
+fail 'port' unless oleh['port']
+
+puts 'ok'
