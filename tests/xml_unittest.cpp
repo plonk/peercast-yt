e323d15458b61d4f2dc2c4ad59caf900655aa51d
Yoteichi <plonk@piano.email.ne.jp>
2017-09-13 13:33:56 +0900

    XMLクラスの単体テストを追加。

--- tests/xml_unittest.cpp
+++ tests/xml_unittest.cpp
@@ -35,3 +35,63 @@ TEST_F(XMLFixture, readCrash)
     ASSERT_TRUE(xml.root != NULL);
     ASSERT_STREQ("%s%s%s%s%s%s%s%s%s%s%s%s", xml.root->getName());
 }
+
+TEST_F(XMLFixture, read_yp4g)
+{
+    std::string input =
+        "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n"
+        "<yp4g>\r\n"
+        "\t<yp name=\"SP\" />\r\n"
+        "\t<host ip=\"254.254.254.254\" port_open=\"1\" speed=\"408\" over=\"0\" />\r\n"
+        "\t<uptest checkable=\"1\" remain=\"0\" />\r\n"
+        "\t<uptest_srv addr=\"bayonet.ddo.jp\" port=\"444\" object=\"/sp/uptest.cgi\" post_size=\"250\" limit=\"3000\" interval=\"15\" enabled=\"1\" />\r\n"
+        "</yp4g>";
+    StringStream mem;
+    mem.str(input);
+
+    XML xml;
+    xml.read(mem);
+    ASSERT_STREQ("yp4g", xml.root->getName());
+    ASSERT_STREQ("SP", xml.findNode("yp")->findAttr("name"));
+    ASSERT_STREQ("408", xml.findNode("host")->findAttr("speed"));
+    ASSERT_EQ(1, xml.findNode("uptest")->findAttrInt("checkable"));
+    ASSERT_STREQ("1", xml.findNode("uptest_srv")->findAttr("enabled"));
+}
+
+TEST_F(XMLFixture, content)
+{
+    StringStream mem("<b>hoge&amp;fuga</b>");
+
+    XML xml;
+    xml.read(mem);
+    ASSERT_STREQ("b", xml.root->getName());
+    ASSERT_STREQ("hoge&amp;fuga", xml.root->getContent()); // 文字実体参照の解決はされない
+}
+
+TEST_F(XMLFixture, emptyContent)
+{
+    StringStream mem("<b></b>");
+
+    XML xml;
+    xml.read(mem);
+    ASSERT_STREQ("b", xml.root->getName());
+    ASSERT_EQ(nullptr, xml.root->getContent()); // 空文字列ではなくNULLが返る
+}
+
+TEST_F(XMLFixture, attribute)
+{
+    StringStream mem("<b attr=\"a&amp;b\"></b>");
+
+    XML xml;
+    xml.read(mem);
+    ASSERT_STREQ("b", xml.root->getName());
+    ASSERT_STREQ("a&amp;b", xml.root->findAttr("attr")); // 文字実体参照の解決はされない
+}
+
+TEST_F(XMLFixture, attribute_singleQuotes)
+{
+    StringStream mem("<b id=\'123\'></b>"); // 単一引用符は動かない
+
+    XML xml;
+    ASSERT_THROW(xml.read(mem), StreamException);
+}
