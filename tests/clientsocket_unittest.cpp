227887f453a1c0d89daf20729beeb212080c9abd
Yoteichi <plonk@piano.email.ne.jp>
2020-01-05 22:23:11 +0900

    ClientSocket::getHostnameのテスト追加。

--- tests/clientsocket_unittest.cpp
+++ tests/clientsocket_unittest.cpp
@@ -10,3 +10,11 @@ TEST_F(ClientSocketFixture, getIP)
     EXPECT_EQ((127<<24 | 1), ClientSocket::getIP("localhost"));
     EXPECT_EQ((127<<24 | 1), ClientSocket::getIP("127.0.0.1"));
 }
+
+TEST_F(ClientSocketFixture, getHostname)
+{
+    char name[256] = "";
+
+    EXPECT_TRUE(ClientSocket::getHostname(name, (127<<24 | 1)));
+    EXPECT_STREQ("localhost", name);
+}
