954d09d031d3dc1b4ae7d5f6aa4dbdb5b1ffd24b
Yoteichi <plonk@piano.email.ne.jp>
2020-01-11 19:40:00 +0900

    Add win32 version.

--- tests/filesystemmapper_unittest.cpp
+++ tests/filesystemmapper_unittest.cpp
@@ -23,9 +23,14 @@ public:
 
 TEST_F(FileSystemMapperFixture, realPath)
 {
+#ifdef _UNIX
     ASSERT_EQ("/", FileSystemMapper::realPath("/"));
     ASSERT_EQ("/tmp", FileSystemMapper::realPath("/tmp"));
     ASSERT_EQ("/root", FileSystemMapper::realPath("/root"));
     ASSERT_EQ("/root", FileSystemMapper::realPath("/tmp/../root"));
+#endif
+#ifdef WIN32
+    ASSERT_STREQ(":\\", FileSystemMapper::realPath("/").c_str() + 1); // The drive letter might vary.
+#endif
     ASSERT_EQ("", FileSystemMapper::realPath("/nonexistent"));
 }
