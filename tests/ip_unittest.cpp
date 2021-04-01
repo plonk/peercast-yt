4996f6edd98f81d4c28e7c80f556534978bbe952
Yoteichi <plonk@piano.email.ne.jp>
2021-04-01 20:24:41 +0900

    IP::tryParse を追加。

--- tests/ip_unittest.cpp
+++ tests/ip_unittest.cpp
@@ -25,3 +25,12 @@ TEST_F(IPFixture, isIPv4Mapped)
     IP ip(127<<24|1);
     ASSERT_TRUE(ip.isIPv4Mapped());
 }
+
+TEST_F(IPFixture, tryParse)
+{
+    IP ip;
+
+    ASSERT_TRUE(IP::tryParse("127.0.0.1", ip));
+    ASSERT_TRUE(IP::tryParse("::1", ip));
+    ASSERT_FALSE(IP::tryParse("foo", ip));
+}
