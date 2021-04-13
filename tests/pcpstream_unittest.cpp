7948ed054407bde66525a5dd871aace35843405d
Yoteichi <plonk@piano.email.ne.jp>
2021-04-13 23:00:52 +0900

    WindowsでSEGVってたテストを修正。

--- tests/pcpstream_unittest.cpp
+++ tests/pcpstream_unittest.cpp
@@ -1,8 +1,14 @@
 #include <gtest/gtest.h>
 
+#include "pcp.h"
+#include "atom.h"
+#include "sstream.h"
+#include "channel.h"
+
 class PCPStreamFixture : public ::testing::Test {
 public:
     PCPStreamFixture()
+        : m_pcp(GnuID())
     {
     }
 
@@ -17,25 +23,32 @@ public:
     ~PCPStreamFixture()
     {
     }
+
+    PCPStream m_pcp;
 };
 
-#include "pcp.h"
-#include "atom.h"
-#include "sstream.h"
-#include "channel.h"
-
-TEST_F(PCPStreamFixture, bigPacket)
+TEST_F(PCPStreamFixture, smallDataPacket)
 {
-    PCPStream pcp{GnuID()};
     auto ch = std::make_shared<Channel>();
-    StringStream mem(std::string({'d','a','t','a',0,0,10,0,0,0,0,0}) + std::string(160 * 1024, 'A'));
+    StringStream mem(std::string({'d','a','t','a',2,0,0,0}) + std::string(2, 'A'));
     AtomStream atom(mem);
     BroadcastState bcs;
     int numc = 1;
 
-    try {
-        pcp.readPktAtoms(ch, atom, numc, bcs);
-    } catch (GeneralException& e ) {
-    }
-    ASSERT_TRUE(true); // とりあえずクラッシュしなければいい。
+    ASSERT_NO_THROW(m_pcp.readPktAtoms(ch, atom, numc, bcs));
+}
+
+TEST_F(PCPStreamFixture, bigDataPacket)
+{
+    // "g++.exe (Rev2, Built by MSYS2 project) 9.2.0" で uniform
+    // initialization に問題があるらしく、以下のようやるとSEGVる。
+    // PCPStream pcp{GnuID()};
+
+    auto ch = std::make_shared<Channel>();
+    StringStream mem(std::string({'d','a','t','a',0,0,10,0}) + std::string(160 * 1024, 'A'));
+    AtomStream atom(mem);
+    BroadcastState bcs;
+    int numc = 1;
+
+    ASSERT_THROW(m_pcp.readPktAtoms(ch, atom, numc, bcs), StreamException);
 }
