86cbe997763c177cd34c34e1d3ccf5e49db10195
Yoteichi <plonk@piano.email.ne.jp>
2021-05-11 21:49:30 +0900

    テストが動くようにした。

--- tests/atom_unittest.cpp
+++ tests/atom_unittest.cpp
@@ -24,21 +24,7 @@ public:
 
 TEST_F(AtomFixture, test)
 {
-    bool sendPort = true;
-    bool sendPing = true;
-    bool sendBCID = true;
-    GnuID sessionID("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    uint16_t port = 7144;
-    GnuID broadcastID("ffffffffffffffffffffffffffffffff");
+    Atom atom(PCP_HELO_PORT, (short) 7144);
 
-    Atom atom(PCP_HELO, {
-        { PCP_HELO_AGENT, PCX_AGENT },
-        { PCP_HELO_VERSION, PCP_CLIENT_VERSION },
-        { PCP_HELO_SESSIONID, sessionID },
-        (sendPort) ? Atom(PCP_HELO_PORT, port) : Atom(),
-        (sendPing) ? Atom(PCP_HELO_PING, port) : Atom(),
-        (sendBCID) ? Atom(PCP_HELO_BCID, broadcastID) : Atom()
-    });
-
-    ASSERT_EQ("hoge", atom.serialize());
+    ASSERT_EQ(std::string({'p','o','r','t',2,0,0,0,(char)0xe8,0x1b}), atom.serialize());
 }
