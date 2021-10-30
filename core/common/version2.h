e290c854a80b06ee361d6e307a233fb37c577b5c
Yoteichi <plonk@piano.email.ne.jp>
2021-10-30 12:16:35 +0900

    バージョン番号を上げた。YT42

--- core/common/version2.h
+++ core/common/version2.h
@@ -26,11 +26,11 @@ static const int PCP_ROOT_VERSION       = 1218;
 
 static const int PCP_CLIENT_MINVERSION  = 1200;
 
-#define PCX_AGENT "PeerCast/0.1218 (YT41)"
-#define PCX_VERSTRING "v0.1218 YT41"
+#define PCX_AGENT "PeerCast/0.1218 (YT42)"
+#define PCX_VERSTRING "v0.1218 YT42"
 
 #define PCP_CLIENT_VERSION_EX_PREFIX "YT"
-static const int PCP_CLIENT_VERSION_EX_NUMBER = 41;
+static const int PCP_CLIENT_VERSION_EX_NUMBER = 42;
 // ------------------------------------------------
 
 #endif
