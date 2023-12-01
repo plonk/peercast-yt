2db55f503ef68f286db1769ff7f27564f6aac25e
Yoteichi <plonk@piano.email.ne.jp>
2023-12-02 02:10:31 +0900

    codepoint_to_utf8 の引数の型をstrtoulの返り値がつっこめるようにunsigned long に変えた。
    オーバーロードは、32ビット環境では unsigned long と uint32_t が同じになるのでできない気がする。

--- core/common/str.cpp
+++ core/common/str.cpp
@@ -201,12 +201,13 @@ std::vector<std::string> split(const std::string& in, const std::string& separat
     }
 }
 
-std::string codepoint_to_utf8(uint32_t codepoint)
+std::string codepoint_to_utf8(unsigned long codepoint)
 {
     std::string res;
 
     if (codepoint >= 0x110000) {
-        throw std::out_of_range("Codepoint out of range [U+0000, U+10FFFF]");
+        auto message = str::format("Codepoint U+%04lX out of valid range [U+0000, U+10FFFF]", codepoint);
+        throw std::out_of_range(message);
     }
 
     if (/* codepoint >= 0 && */ codepoint <= 0x7f) {
