f46d8e82acf83a401f5868289428de7147711b23
Yoteichi <plonk@piano.email.ne.jp>
2024-02-10 01:00:16 +0900

    sleepコマンドを追加。

--- core/common/commands.cpp
+++ core/common/commands.cpp
@@ -43,6 +43,7 @@ s_commands = {
               { "pid", Commands::pid },
               { "expr", Commands::expr },
               { "shutdown", Commands::shutdown },
+              { "sleep", Commands::sleep },
 };
 
 void Commands::system(Stream& stream, const std::string& _cmdline, std::function<bool()> cancel)
@@ -65,6 +66,22 @@ void Commands::system(Stream& stream, const std::string& _cmdline, std::function
     } catch (GeneralException& e) {
         stream.writeLineF("Error: %s", e.what());
     }
+}
+
+#include <cstdlib>
+void Commands::sleep(Stream& stream, const std::vector<std::string>& argv, std::function<bool()> cancel)
+{
+    std::map<std::string, bool> options;
+    std::vector<std::string> positionals;
+    std::tie(options, positionals) = parse_options(argv, {"--help"});
+
+    if (positionals.size() != 1 || options.count("--help")) {
+        stream.writeLine("Usage: sleep NUMBER");
+        stream.writeLine("Pause for NUMBER seconds.");
+        return;
+    }
+    double seconds = std::atof(positionals[0].c_str());
+    sys->sleep(seconds * 1000);
 }
 
 #include "servmgr.h"
