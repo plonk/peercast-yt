6ade9dc45dd6e761b11c734e4b8ad5d6870a05c2
Yoteichi <plonk@piano.email.ne.jp>
2024-02-13 21:31:13 +0900

    Regexp::escape と Regexp::grep 作った。

--- core/common/regexp.h
+++ core/common/regexp.h
@@ -12,6 +12,9 @@ public:
     Regexp(const Regexp& orig);
     ~Regexp();
 
+    static std::string escape(const std::string&);
+    static std::vector<std::string> grep(const Regexp&, const std::vector<std::string>&);
+
     std::vector<std::string> exec(const std::string& str) const;
     bool matches(const std::string& str) const;
 
