42021ff7133c5e906d2c80fe795e9727e064eb75
Yoteichi <plonk@piano.email.ne.jp>
2024-02-16 22:26:08 +0900

    Revert "SSL ハンドシェイクを無効にした。"
    This reverts commit 5ea97a165ed04e7f092a0e88d6fc42428946c873.

--- core/common/servhs.cpp
+++ core/common/servhs.cpp
@@ -829,9 +829,6 @@ void Servent::handshakeIncoming()
 {
     setStatus(S_HANDSHAKE);
 
-#if 0
-    // SSL ハンドシェイクを検出したらSSL通信を行う。
-    // ※ サーバー証明書を設定する機能を作るまで無効にする。
     if (sock->readReady(sock->readTimeout)) {
         char c = sock->peekChar();
         LOG_TRACE("peekChar -> %d", (unsigned char) c);
@@ -839,7 +836,6 @@ void Servent::handshakeIncoming()
             sock = SslClientSocket::upgrade(sock);
         }
     }
-#endif
 
     char buf[8192];
 
